f2866cdaa87ed086eb2799334f736ee01e4d1c9b
priyasakthi <141812382+290704@users.noreply.github.com>
2025-02-12 18:38:45 +0530

    Add files via upload

---
+++ Day7/MyAbstractClass.java
@@ -0,0 +1,14 @@
+package com.celcom.pack1;
+
+public abstract class MyAbstractClass {
+       public void myMethod1() {
+    	   System.out.println("My Method one");
+       }
+       abstract  void myMethod2();
+	
+
+	
+
+	}
+
+
---
+++ Day7/MyConcreteClass.java
@@ -0,0 +1,15 @@
+package com.celcom.pack1;
+
+public class MyConcreteClass extends MyAbstractClass {
+	public MyConcreteClass(){
+		
+	}
+    public  void myMethod2() {
+    	 System.out.println("My method two");
+     }
+	
+		
+
+	}
+
+

bea733e25df5a91365197e24767c188f6694228e
priyasakthi <141812382+290704@users.noreply.github.com>
2025-02-13 18:09:04 +0530

    Create StringEx1.java

---
+++ Day8/StringEx1.java
@@ -0,0 +1,20 @@
+package com.celcom.day8;
+
+public class StringEx1 {
+
+	public static void main(String[] args) {
+		String s1="Priya";
+		
+		String s2=new String("java");
+		
+		char ch[]= {'H','i'};
+		String s3=new String(ch);
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		
+
+	}
+
+}

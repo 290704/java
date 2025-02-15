546d9ca0d4bf72ba2964ac2e91afbee5255dcc40
priyasakthi <141812382+290704@users.noreply.github.com>
2025-02-15 11:41:06 +0530

    Add files via upload

---
+++ Saturday-Assign1-feb[08-02-25]/AssignEx6.java
@@ -0,0 +1,39 @@
+package Assignment1;
+class Employee2{
+	String name;
+	String jobTitle;
+	double salary;
+	
+	Employee2(String name,String jobTitle,double salary){
+		this.name=name;
+		this.jobTitle=jobTitle;
+		this.salary=salary;
+	}
+	
+	double calculateSalaryIncrease(double percentage) {
+		return (salary*percentage)/percentage;
+	}
+	
+	void  updateSalary(double percentage) {
+		double increase=calculateSalaryIncrease(percentage);
+		this.salary+=increase;
+		System.out.println("salary increment successfully:"+this.salary);
+	}
+	void employeeDetail() {
+		System.out.println("Employee Name:"+name);
+		System.out.println("Employee jobTitle:"+jobTitle);
+		System.out.println("Employee salary:"+salary);
+		
+	}
+}
+public class AssignEx6 {
+	public static void main(String args[]) {
+		Employee2 emp=new Employee2("Priya","Software Engineer",35000d);
+		emp.employeeDetail();
+		emp.updateSalary(10);
+		
+		
+		
+		
+	}
+}
